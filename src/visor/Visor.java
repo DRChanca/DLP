d86181c29d4359ed4c7765f1220543b4dd4bef4e
david <davidrch03@gmail.com>
2025-03-12 19:37:47 +0100

    Se hace el recorrido del visitor y imprimo todo en orden

--- src/visor/Visor.java
+++ src/visor/Visor.java
@@ -1,5 +1,7 @@
 package visor;
 
+import java.nio.file.AccessDeniedException;
+
 import ast.AST;
 import ast.Argumento;
 import ast.Declaracion;
@@ -59,7 +61,11 @@ public class Visor extends DefaultVisitor {
 	public Object visit(Declaracionstructs declaracionstructs, Object param) {
 
 		// declaracionstructs.getDeclaracions().forEach(declaracion -> declaracion.accept(this, param));
+		System.out.println();
+		System.out.println("struct "+declaracionstructs.getNombre()+" {");
 		super.visit(declaracionstructs, param);
+		System.out.println();
+		System.out.println("}");
 
 		return null;
 	}
@@ -67,9 +73,9 @@ public class Visor extends DefaultVisitor {
 	// class Declaracionglobales(Declaracion declaracion)
 	@Override
 	public Object visit(Declaracionglobales declaracionglobales, Object param) {
-
-		// declaracionglobales.getDeclaracion().accept(this, param);
-		super.visit(declaracionglobales, param);
+		System.out.print("var"); 
+		declaracionglobales.getDeclaracion().accept(this, param);
+		//super.visit(declaracionglobales, param);
 
 		return null;
 	}
@@ -78,11 +84,20 @@ public class Visor extends DefaultVisitor {
 	@Override
 	public Object visit(Declaracionfuncion declaracionfuncion, Object param) {
 
-		// declaracionfuncion.getArgumento().forEach(argumento -> argumento.accept(this, param));
-		// declaracionfuncion.getTipo().ifPresent(tipo -> tipo.accept(this, param));
-		// declaracionfuncion.getVariablesLocales().forEach(variablesLocales -> variablesLocales.accept(this, param));
-		// declaracionfuncion.getSentencias().forEach(sentencia -> sentencia.accept(this, param));
-		super.visit(declaracionfuncion, param);
+		System.out.print(declaracionfuncion.getNombre()+ "(");
+		declaracionfuncion.getArgumento().forEach(argumento -> argumento.accept(this, param));
+		System.out.print(") : ");
+		declaracionfuncion.getTipo().ifPresent(tipo -> tipo.accept(this, param));
+		System.out.println( "{ ");
+		declaracionfuncion.getVariablesLocales().forEach(variablesLocales -> variablesLocales.accept(this, param));
+		declaracionfuncion.getSentencias().forEach(sentencia -> sentencia.accept(this, param));
+		System.out.println();
+		System.out.println("}");
+		
+		
+		
+		
+		//super.visit(declaracionfuncion, param);
 
 		return null;
 	}
@@ -90,9 +105,10 @@ public class Visor extends DefaultVisitor {
 	// class VariablesLocales(String IDENT, Tipo tipo)
 	@Override
 	public Object visit(VariablesLocales variablesLocales, Object param) {
-
-		// variablesLocales.getTipo().accept(this, param);
-		super.visit(variablesLocales, param);
+		System.out.print("var "+ variablesLocales.getIDENT()+":");
+		variablesLocales.getTipo().accept(this, param);
+		System.out.println();
+		//super.visit(variablesLocales, param);
 
 		return null;
 	}
@@ -100,9 +116,10 @@ public class Visor extends DefaultVisitor {
 	// class Argumento(String IDENT, Tipo tipo)
 	@Override
 	public Object visit(Argumento argumento, Object param) {
-
-		// argumento.getTipo().accept(this, param);
-		super.visit(argumento, param);
+		System.out.print( argumento.getIDENT()+":");
+		
+		argumento.getTipo().accept(this, param);
+		//super.visit(argumento, param);
 
 		return null;
 	}
@@ -110,9 +127,9 @@ public class Visor extends DefaultVisitor {
 	// class Declaracion(String IDENT, Tipo tipo)
 	@Override
 	public Object visit(Declaracion declaracion, Object param) {
-
-		// declaracion.getTipo().accept(this, param);
-		super.visit(declaracion, param);
+		System.out.print(" "+declaracion.getIDENT()+": ");
+		declaracion.getTipo().accept(this, param);
+		//super.visit(declaracion, param);
 
 		return null;
 	}
@@ -120,9 +137,10 @@ public class Visor extends DefaultVisitor {
 	// class PrintSentencia(List<Expression> expressions)
 	@Override
 	public Object visit(PrintSentencia printSentencia, Object param) {
-
-		// printSentencia.getExpressions().forEach(expression -> expression.accept(this, param));
-		super.visit(printSentencia, param);
+		System.out.print("print ");
+		printSentencia.getExpressions().forEach(expression -> expression.accept(this, param));
+		System.out.println(";");
+		//super.visit(printSentencia, param);
 
 		return null;
 	}
@@ -130,9 +148,10 @@ public class Visor extends DefaultVisitor {
 	// class ReadSentencia(List<Expression> expressions)
 	@Override
 	public Object visit(ReadSentencia readSentencia, Object param) {
-
-		// readSentencia.getExpressions().forEach(expression -> expression.accept(this, param));
-		super.visit(readSentencia, param);
+		 System.out.print("read ");
+		 readSentencia.getExpressions().forEach(expression -> expression.accept(this, param));
+		 System.out.println(";");
+		//super.visit(readSentencia, param);
 
 		return null;
 	}
@@ -140,9 +159,10 @@ public class Visor extends DefaultVisitor {
 	// class PrintspSentencia(List<Expression> expressions)
 	@Override
 	public Object visit(PrintspSentencia printspSentencia, Object param) {
-
-		// printspSentencia.getExpressions().forEach(expression -> expression.accept(this, param));
-		super.visit(printspSentencia, param);
+		System.out.print("printSp ");
+		 printspSentencia.getExpressions().forEach(expression -> expression.accept(this, param));
+		 System.out.println(";");
+		//super.visit(printspSentencia, param);
 
 		return null;
 	}
@@ -150,9 +170,10 @@ public class Visor extends DefaultVisitor {
 	// class PrintlnSentencia(List<Expression> expressions)
 	@Override
 	public Object visit(PrintlnSentencia printlnSentencia, Object param) {
-
-		// printlnSentencia.getExpressions().forEach(expression -> expression.accept(this, param));
-		super.visit(printlnSentencia, param);
+		System.out.print("printLn ");
+		 printlnSentencia.getExpressions().forEach(expression -> expression.accept(this, param));
+		 System.out.println(";");
+		 //super.visit(printlnSentencia, param);
 
 		return null;
 	}
@@ -160,9 +181,10 @@ public class Visor extends DefaultVisitor {
 	// class FuncionSentencia(String nombre, List<Expression> argumento)
 	@Override
 	public Object visit(FuncionSentencia funcionSentencia, Object param) {
-
-		// funcionSentencia.getArgumento().forEach(expression -> expression.accept(this, param));
-		super.visit(funcionSentencia, param);
+		System.out.print(funcionSentencia.getNombre() + "(");
+		 funcionSentencia.getArgumento().forEach(expression -> expression.accept(this, param));
+		 System.out.println(") ; ");
+		 //super.visit(funcionSentencia, param);
 
 		return null;
 	}
@@ -171,9 +193,11 @@ public class Visor extends DefaultVisitor {
 	@Override
 	public Object visit(AsignacionSentencia asignacionSentencia, Object param) {
 
-		// asignacionSentencia.getLeft().accept(this, param);
-		// asignacionSentencia.getExpression().accept(this, param);
-		super.visit(asignacionSentencia, param);
+		 asignacionSentencia.getLeft().accept(this, param);
+		 System.out.print(" = ");
+		 asignacionSentencia.getExpression().accept(this, param);
+		 System.out.println(";");
+		//super.visit(asignacionSentencia, param);
 
 		return null;
 	}
@@ -181,9 +205,10 @@ public class Visor extends DefaultVisitor {
 	// class ReturnSentencia(Optional<Expression> expression)
 	@Override
 	public Object visit(ReturnSentencia returnSentencia, Object param) {
-
-		// returnSentencia.getExpression().ifPresent(expression -> expression.accept(this, param));
-		super.visit(returnSentencia, param);
+		System.out.print("return ");
+		 returnSentencia.getExpression().ifPresent(expression -> expression.accept(this, param));
+		 System.out.println("; ");
+		//super.visit(returnSentencia, param);
 
 		return null;
 	}
@@ -191,11 +216,18 @@ public class Visor extends DefaultVisitor {
 	// class IfSentencia(Expression condicion, List<Sentencia> entonces, List<Sentencia> otro)
 	@Override
 	public Object visit(IfSentencia ifSentencia, Object param) {
-
-		// ifSentencia.getCondicion().accept(this, param);
-		// ifSentencia.getEntonces().forEach(sentencia -> sentencia.accept(this, param));
-		// ifSentencia.getOtro().forEach(sentencia -> sentencia.accept(this, param));
-		super.visit(ifSentencia, param);
+		System.out.print("if (");
+		ifSentencia.getCondicion().accept(this, param);
+		System.out.println(")  {");
+		ifSentencia.getEntonces().forEach(sentencia -> sentencia.accept(this, param));
+		System.out.println("}");
+		
+		if(ifSentencia.getOtro().size() >= 1) {
+			System.out.println( "else {");
+			ifSentencia.getOtro().forEach(sentencia -> sentencia.accept(this, param));
+			System.out.println("}");
+		}
+		//super.visit(ifSentencia, param);
 
 		return null;
 	}
@@ -203,10 +235,12 @@ public class Visor extends DefaultVisitor {
 	// class WhileSentencia(Expression condicion, List<Sentencia> entonces)
 	@Override
 	public Object visit(WhileSentencia whileSentencia, Object param) {
-
-		// whileSentencia.getCondicion().accept(this, param);
-		// whileSentencia.getEntonces().forEach(sentencia -> sentencia.accept(this, param));
-		super.visit(whileSentencia, param);
+		System.out.print("while (");
+		whileSentencia.getCondicion().accept(this, param);
+		System.out.println(" ) {");
+		whileSentencia.getEntonces().forEach(sentencia -> sentencia.accept(this, param));
+		System.out.println("}");
+		//super.visit(whileSentencia, param);
 
 		return null;
 	}
@@ -214,38 +248,39 @@ public class Visor extends DefaultVisitor {
 	// class IntExpresion(int intValue)
 	@Override
 	public Object visit(IntExpresion intExpresion, Object param) {
-
+		System.out.print(intExpresion.getIntValue());
 		return null;
 	}
 
 	// class RealExpresion(double doubleValue)
 	@Override
 	public Object visit(RealExpresion realExpresion, Object param) {
-
+		System.out.print(realExpresion.getDoubleValue());
 		return null;
 	}
 
 	// class IdentificadorExpresion(String name)
 	@Override
 	public Object visit(IdentificadorExpresion identificadorExpresion, Object param) {
-
+		System.out.print(identificadorExpresion.getName());
 		return null;
 	}
-
 	// class CharExpresion(char charValue)
 	@Override
 	public Object visit(CharExpresion charExpresion, Object param) {
-
+		System.out.print(charExpresion.getCharValue());
 		return null;
 	}
 
 	// class AccessoArrayExpresion(Expression acceso, Expression indice)
 	@Override
 	public Object visit(AccessoArrayExpresion accessoArrayExpresion, Object param) {
-
-		// accessoArrayExpresion.getAcceso().accept(this, param);
-		// accessoArrayExpresion.getIndice().accept(this, param);
-		super.visit(accessoArrayExpresion, param);
+		
+		 accessoArrayExpresion.getAcceso().accept(this, param);
+		 System.out.print("[");
+		 accessoArrayExpresion.getIndice().accept(this, param);
+		 System.out.print("]");
+		//super.visit(accessoArrayExpresion, param);
 
 		return null;
 	}
@@ -253,9 +288,10 @@ public class Visor extends DefaultVisitor {
 	// class ParentesisExpresion(Expression expression)
 	@Override
 	public Object visit(ParentesisExpresion parentesisExpresion, Object param) {
-
-		// parentesisExpresion.getExpression().accept(this, param);
-		super.visit(parentesisExpresion, param);
+		System.out.print("(");
+		parentesisExpresion.getExpression().accept(this, param);
+		System.out.print(")");
+		//super.visit(parentesisExpresion, param);
 
 		return null;
 	}
@@ -263,10 +299,11 @@ public class Visor extends DefaultVisitor {
 	// class CastExpresion(Tipo tipo, Expression expression)
 	@Override
 	public Object visit(CastExpresion castExpresion, Object param) {
-
-		// castExpresion.getTipo().accept(this, param);
-		// castExpresion.getExpression().accept(this, param);
-		super.visit(castExpresion, param);
+		System.out.print("<");
+		castExpresion.getTipo().accept(this, param);
+		System.out.println(">");
+		castExpresion.getExpression().accept(this, param);
+		//super.visit(castExpresion, param);
 
 		return null;
 	}
@@ -274,9 +311,9 @@ public class Visor extends DefaultVisitor {
 	// class NegacionExpresion(Expression expression)
 	@Override
 	public Object visit(NegacionExpresion negacionExpresion, Object param) {
-
-		// negacionExpresion.getExpression().accept(this, param);
-		super.visit(negacionExpresion, param);
+		System.out.print("!");
+		negacionExpresion.getExpression().accept(this, param);
+		//super.visit(negacionExpresion, param);
 
 		return null;
 	}
@@ -285,9 +322,10 @@ public class Visor extends DefaultVisitor {
 	@Override
 	public Object visit(ArithmeticExpresion arithmeticExpresion, Object param) {
 
-		// arithmeticExpresion.getLeft().accept(this, param);
-		// arithmeticExpresion.getRight().accept(this, param);
-		super.visit(arithmeticExpresion, param);
+		 arithmeticExpresion.getLeft().accept(this, param);
+		 System.out.print(arithmeticExpresion.getOperator());
+		 arithmeticExpresion.getRight().accept(this, param);
+		//super.visit(arithmeticExpresion, param);
 
 		return null;
 	}
@@ -296,9 +334,10 @@ public class Visor extends DefaultVisitor {
 	@Override
 	public Object visit(LogicExpression logicExpression, Object param) {
 
-		// logicExpression.getLeft().accept(this, param);
-		// logicExpression.getRight().accept(this, param);
-		super.visit(logicExpression, param);
+		logicExpression.getLeft().accept(this, param);
+		System.out.print(logicExpression.getOperator());
+		logicExpression.getRight().accept(this, param);
+		//super.visit(logicExpression, param);
 
 		return null;
 	}
@@ -306,9 +345,11 @@ public class Visor extends DefaultVisitor {
 	// class AcederCap(Expression left, String right)
 	@Override
 	public Object visit(AcederCap acederCap, Object param) {
-
-		// acederCap.getLeft().accept(this, param);
-		super.visit(acederCap, param);
+	
+		acederCap.getLeft().accept(this, param);
+		
+		System.out.print("."+acederCap.getRight());
+		//super.visit(acederCap, param);
 
 		return null;
 	}
@@ -317,8 +358,8 @@ public class Visor extends DefaultVisitor {
 	@Override
 	public Object visit(FuncionExpresion funcionExpresion, Object param) {
 
-		// funcionExpresion.getArgumentos().forEach(expression -> expression.accept(this, param));
-		super.visit(funcionExpresion, param);
+		 funcionExpresion.getArgumentos().forEach(expression -> expression.accept(this, param));
+		//super.visit(funcionExpresion, param);
 
 		return null;
 	}
@@ -326,21 +367,21 @@ public class Visor extends DefaultVisitor {
 	// class IntTipo(String name)
 	@Override
 	public Object visit(IntTipo intTipo, Object param) {
-
+		System.out.print(intTipo.getName()+"; ");
 		return null;
 	}
 
 	// class FloatTipo(String name)
 	@Override
 	public Object visit(FloatTipo floatTipo, Object param) {
-
+		System.out.print(floatTipo.getName());
 		return null;
 	}
 
 	// class CharTipo(String name)
 	@Override
 	public Object visit(CharTipo charTipo, Object param) {
-
+		System.out.print(charTipo.getName());
 		return null;
 	}
 
@@ -348,8 +389,12 @@ public class Visor extends DefaultVisitor {
 	@Override
 	public Object visit(ArrayTipo arrayTipo, Object param) {
 
-		// arrayTipo.getTipo().accept(this, param);
-		super.visit(arrayTipo, param);
+		
+		//super.visit(arrayTipo, param);
+		
+		
+		System.out.print("["+ arrayTipo.getTamArray()+"]");
+		arrayTipo.getTipo().accept(this, param);
 
 		return null;
 	}
@@ -357,7 +402,7 @@ public class Visor extends DefaultVisitor {
 	// class StringTipo(String name)
 	@Override
 	public Object visit(StringTipo stringTipo, Object param) {
-
+		System.out.println(stringTipo.getName());
 		return null;
 	}
 
